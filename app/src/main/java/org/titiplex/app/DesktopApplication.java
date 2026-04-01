1261aece9020041cec835e69df9c2700bd2f11af
Titiplex <107863779+Titiplex@users.noreply.github.com>
2026-04-01 12:08:12 -0400

    working

--- app/src/main/java/org/titiplex/app/DesktopApplication.java
+++ app/src/main/java/org/titiplex/app/DesktopApplication.java
@@ -1,20 +1,10 @@
 package org.titiplex.app;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Import;
 import org.titiplex.backend.BackendApplication;
 
-@SpringBootApplication
-@ComponentScan(
-        basePackages = {
-                "org.titiplex.app",
-                "org.titiplex.backend"
-        },
-        excludeFilters = @ComponentScan.Filter(
-                type = FilterType.ASSIGNABLE_TYPE,
-                classes = BackendApplication.class
-        )
-)
+@SpringBootApplication(scanBasePackages = "org.titiplex.app")
+@Import(BackendApplication.class)
 public class DesktopApplication {
 }
